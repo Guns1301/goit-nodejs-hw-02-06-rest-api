a08a6e962fcbc7431617ac4ecd77b8668c286094
Guns1301 <67137938+Guns1301@users.noreply.github.com>
2021-10-02 00:52:16 +0300

    update eslint config readme

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,9 +4,13 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: ['standard', 'prettier', 'plugin:json/recommended'],
+  extends: ["standard", "prettier", "plugin:json/recommended"],
   parserOptions: {
     ecmaVersion: 12,
   },
-  rules: {},
+  rules: {
+    "comma-dangle": "off",
+    "space-before-function-paren": "off",
+  },
+  quotes: [2, "double", { avoidEscape: true }, "error", "single"],
 };
